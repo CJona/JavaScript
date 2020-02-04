8c19e1f815eea055dd9eedb9f54793b7297d273e
CJona <54817036+CJona@users.noreply.github.com>
2020-02-04 09:49:40 +0100

    H2

---
+++ H2/2.1/Aanpassen.js
@@ -0,0 +1,5 @@
+
+document.getElementById("paginatitel").innerText = "Dit de paginatitel";
+document.getElementById("subkop").innerText = "Dit de subkop";
+document.getElementById("tijger").src = "img/Tijger Wallpap.jpeg";
+document.getElementById("Hond").src = "img/pexels-photo-733416.jpeg";
